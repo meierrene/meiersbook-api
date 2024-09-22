d765f99d59102924fe6b94a9feeeadaf2c48694f
René Meier <renemeier.de@gmail.com>
2024-09-21 22:16:54 -0300

    Small fixes

--- utils/options.js
+++ utils/options.js
@@ -1,3 +1,21 @@
-exports.pathPostImage = 'uploads/postsImages';
-exports.pathUserImage = 'uploads/userImages';
-exports.newImage = 'newImage.jpeg';
+const fs = require('fs');
+const path = require('path');
+
+// Define paths
+const pathPostImage = path.join(__dirname, 'uploads/postsImages/');
+const pathUserImage = path.join(__dirname, 'uploads/userImages/');
+const newImage = 'newImage.jpeg';
+
+// Ensure directories exist or create them
+[pathPostImage, pathUserImage].forEach(dir => {
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+  }
+});
+
+// Export paths
+module.exports = {
+  pathPostImage,
+  pathUserImage,
+  newImage,
+};
