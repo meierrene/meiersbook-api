eef2a0a94b0ca6564cf74fe06c40155b2b17b464
René Meier <renemeier.de@gmail.com>
2024-09-21 23:42:08 -0300

    Small fixes

--- app.js
+++ app.js
@@ -2,23 +2,15 @@ const express = require('express');
 const postRouter = require('./routes/postRoutes');
 const userRouter = require('./routes/userRoutes');
 const ErrorThrower = require('./utils/ErrorThrower');
+// const errorController = require('./controllers/errorController');
 const path = require('path');
 const app = express();
 
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 
-// Serve static files for post images and user images
-app.use(
-  '/uploads/postsImages',
-  express.static(path.join(__dirname, 'uploads', 'postsImages'))
-);
-app.use(
-  '/uploads/userImages',
-  express.static(path.join(__dirname, 'uploads', 'userImages'))
-);
+app.use('/uploads/', express.static(path.join(__dirname, 'uploads')));
 
-// CORS settings
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
@@ -29,11 +21,9 @@ app.use((req, res, next) => {
   next();
 });
 
-// Routes
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/posts', postRouter);
 
-// Error handler for unknown routes
 app.all('*', (req, res, next) => {
   next(new ErrorThrower(`Can't find ${req.originalUrl} on this server!`, 404));
 });
--- utils/options.js
+++ utils/options.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 const path = require('path');
 
 // Define paths
-const pathPostImage = path.join(__dirname, 'uploads/postsImages/');
-const pathUserImage = path.join(__dirname, 'uploads/userImages/');
+const pathPostImage = path.join(__dirname, '..', 'uploads/postsImages/');
+const pathUserImage = path.join(__dirname, '..', 'uploads/userImages/');
 const newImage = 'newImage.jpeg';
 
 // Ensure directories exist or create them
