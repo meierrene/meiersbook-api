23821815c88089d8274b9b83e3107b41b005af97
unknown <renemeier.de@gmail.com>
2025-01-09 15:48:28 -0300

    Thumbnail adjustment

--- utils/options.js
+++ utils/options.js
@@ -1,4 +1,4 @@
-const thumbnailSettings = { size: 600, quality: 80 };
+const thumbnailSettings = { size: 600, quality: 70 };
 const newImage = 'newImage.jpeg';
 const getBucket = Model => {
   const bucket = `${Model.modelName.toLowerCase()}-images${
